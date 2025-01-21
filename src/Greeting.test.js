baf7f9214709dc43f0ba4437a28bcec8d52f886b
Christophar Antony <christopharantony@gmail.com>
2025-01-22 00:25:28 +0530

    Wxplore grouping using `describe` block for better structure

--- src/Greeting.test.js
+++ src/Greeting.test.js
@@ -1,9 +1,11 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
 
-test("renders Hello World as a text", () => {
-  render(<Greeting />);
+describe("Greeting Component", () => {
+  test("renders Hello World as a text", () => {
+    render(<Greeting />);
 
-  const helloWorldElement = screen.getByText("Hello World", { exact: false }); // exact false is for checking sub-strings and case insensitive
-  expect(helloWorldElement).toBeInTheDocument();
+    const helloWorldElement = screen.getByText("Hello World", { exact: false }); // exact false is for checking sub-strings and case insensitive
+    expect(helloWorldElement).toBeInTheDocument();
+  });
 });
