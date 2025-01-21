f2d5dd3ef3698276f9b028f174389acb4ded7ea4
Christophar Antony <christopharantony@gmail.com>
2025-01-22 00:22:08 +0530

    First test

---
+++ src/Greeting.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Greeting = () => {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>It's good to see you 🫶</p>
+    </div>
+  );
+};
+
+export default Greeting;
